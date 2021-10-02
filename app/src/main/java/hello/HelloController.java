30b15a85e4788c9ed4a18cc74cd06f12a6c256d1
AsbelJose <asbeljose@hotmail.com>
2021-10-02 16:52:05 -0500

    english again

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Saludos desde el Spring Boot!";
+        return "Hello from Spring Boot!";
     }   
 }
